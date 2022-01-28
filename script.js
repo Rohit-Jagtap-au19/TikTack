686e9b08e9c65e189642ae50fee8709c0e2ec2b5
Rohit <prjagtap90@gmail.com>
2022-01-28 23:04:46 +0530

    update

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ let turn = "X";
 let Gameover = false
 
 // function to change turn
-const changeturn = ()=> {
-    return turn === "X"? "0": "X"
+const changeturn = () => {
+    return turn === "X" ? "0" : "X"
 }
 // functionto check for a win 
-const checkwin = () =>{
+const checkwin = () => {
     let boxtexts = document.getElementsByClassName('boxtext');
     let wins = [
         [0, 1, 2, 0, 5, 0],
@@ -23,44 +23,59 @@ const checkwin = () =>{
         [0, 4, 8, -1, 15, 45],
         [2, 4, 6, -1, 15, 135],
     ]
-    wins.forEach(e =>{
-       if((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !=="") ){
-           document.querySelector('.info').innerText ="WINNER " + boxtexts[e[0]].innerText 
-           Gameover = true
-           document.querySelector('.imagebox').getElementsByTagName('img')[0].style.width = "200px"
-           document.querySelector('.line').style.width = "32vw"
-           document.querySelector('.line').style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-           music.play()
+    wins.forEach(e => {
+        if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== "")) {
+            document.querySelector('.info').innerText = "WINNER " + boxtexts[e[0]].innerText
+            Gameover = true
+            document.querySelector('.imagebox').getElementsByTagName('img')[0].style.width = "200px"
+            document.querySelector('.line').style.width = "32vw"
+            document.querySelector('.line').style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            music.play()
 
-       }
+            // auto restes game after 4 sec
+
+            setTimeout(() => {
+                let boxtexts = document.querySelectorAll('.boxtext');
+                Array.from(boxtexts).forEach(element => {
+                    element.innerText = ""
+                })
+                turn = "X";
+                Gameover = false;
+                document.querySelector('.line').style.width = "0vw"
+                document.getElementsByClassName('info')[0].innerText = "Turn for " + turn;
+                document.querySelector('.imagebox').getElementsByTagName('img')[0].style.width = "0px"
+                music.pause()
+            }, 4000);
+        }
     })
 }
 // game
 let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
+Array.from(boxes).forEach(element => {
     let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click', ()=>{
-        if(boxtext.innerText === ''){
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '') {
             boxtext.innerText = turn;
             turn = changeturn();
             audioturn.play();
             checkwin();
-            if(!Gameover){
+            if (!Gameover) {
                 document.getElementsByClassName('info')[0].innerText = "Turn for " + turn;
             }
         }
-    })  
+    })
 })
 // add onclick listner to reset button
-reset.addEventListener('click', ()=>{
+reset.addEventListener('click', () => {
     let boxtexts = document.querySelectorAll('.boxtext');
-    Array.from(boxtexts).forEach(element =>{
+    Array.from(boxtexts).forEach(element => {
         element.innerText = ""
     })
     turn = "X";
     Gameover = false;
-    document.querySelector('.line').style.width = "0vw" 
+    document.querySelector('.line').style.width = "0vw"
     document.getElementsByClassName('info')[0].innerText = "Turn for " + turn;
     document.querySelector('.imagebox').getElementsByTagName('img')[0].style.width = "0px"
-
+    music.pause()
 })
+
